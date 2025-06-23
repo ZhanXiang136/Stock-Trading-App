03e4306d3a0417ab36f6d00fb61fa0498cd05bf7
Zhan Xiang Zheng <zhanxiangzheng136@gmail.com>
2025-06-22 20:29:01 -0400

    added required python files for project setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Stock Trading AI App',
+    version='0.1.0',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=requirements,
+    description='A Flask application for analyzing Reddit posts and generating stock trading signals based on sentiment analysis',
+    entry_points={
+        'console_scripts': [
+            'web: gunicorn src.app:app',  
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Framework :: Flask',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7',
+)
--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import os
 import datetime as dt
 from reddit_sentiment_pipeline.scrape import fetch_recent_posts
 from reddit_sentiment_pipeline.sentiment_utils import Sentiment_Analyzer
-from reddit_sentiment_pipeline.fine_tune import fine_tune_from_csv
+from reddit_sentiment_pipeline.fine_tune import download_model_from_huggingface
 from trading_engine.alpaca_trade import submit_order, get_position
 
 from fastapi import FastAPI
@@ -29,17 +29,17 @@ def performance():
     return RedirectResponse(url="/docs")
 
 
-MODEL_DIR = os.getenv("SENTIMENT_MODEL_PATH", "src/model")
+MODEL_DIR = os.getenv("SENTIMENT_MODEL_PATH", "src//model")
 
-def ensure_model_trained():
+def ensure_mode_exist():
     if not os.path.exists(MODEL_DIR):
         print("Model not found. Fine-tuning model using training data...")
-        fine_tune_from_csv(TRAIN_CSV)
+        download_model_from_huggingface()
     else:
         print(f"Using existing model at: {MODEL_DIR}")
 
-if __name__ == "__main__":
-    ensure_model_trained()
+def main():
+    ensure_mode_exist()
 
     print("Fetching Reddit posts...")
     hours = 72 if dt.datetime.now().weekday() == 0 else 24
@@ -68,9 +68,12 @@ if __name__ == "__main__":
     for ticker, signal in signals.items():
         print(f"{ticker}: {signal}")
 
-    # print("Executing trades...")
-    # for ticker, signal in signals.items():
-    #     if signal == "BUY":
-    #         submit_order(ticker, qty=1, side="buy")
-    #     elif signal == "SELL":
-    #         submit_order(ticker, qty=1, side="sell")
+    print("Executing trades...")
+    for ticker, signal in signals.items():
+        if signal == "BUY":
+            submit_order(ticker, qty=1, side="buy")
+        elif signal == "SELL":
+            submit_order(ticker, qty=1, side="sell")
+
+if __name__ == "__main__":
+    main()
